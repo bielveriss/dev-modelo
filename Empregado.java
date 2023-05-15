a0988f91de49f7ffc701cdd384c14b5549d7f533
Gabriel <gverissimo252@gmail.com>
2023-05-15 18:17:39 -0300

    Criação da classe de teste e inserção do tipo na classe empregado.java

--- Empregado.java
+++ Empregado.java
@@ -26,14 +26,27 @@ public class Empregado {
     public String getNome() {
         return nome;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
+
     public int getIdade() {
         return idade;
     }
+
     public void setIdade(int idade) {
         this.idade = idade;
     }
+
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+    
     
 }
